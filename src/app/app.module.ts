4e5f177c865168c6929a7636296733983a8eec76
Mitchell Irvin <mitchell.k.irvin@gmail.com>
2018-02-27 13:05:00 -0500

    Added angular material and toolbar for header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -19,7 +20,8 @@ import { Service } from './components/service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule
+    HttpModule,
+    MatToolbarModule
   ],
   providers: [Service],
   bootstrap: [AppComponent]
